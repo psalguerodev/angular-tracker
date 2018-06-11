0692ae598adefd2ff0124f4337d840c62a241049
Patrick Salguero <patrick.salguero.avalos@gmail.com>
2018-06-10 20:00:08 -0500

    se modifica la configuracion

---
+++ api/config/configuration.js
@@ -0,0 +1,7 @@
+module.exports = {
+    AUTHOR : 'Patrick Salguero',
+    PORT : 9000,
+    KEYCRYPT : 'cHNhbGd1ZXJv', //psalguero
+    REGEX_PASSWORD : /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/ ,
+    PATHUPLOAD : './api/uploads/'
+}
