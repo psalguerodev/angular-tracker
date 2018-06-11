bd42df69668ebbbc1674e9ade1ef2d3f19ba5e8a
Patrick Salguero <patrick.salguero.avalos@gmail.com>
2018-06-10 20:01:04 -0500

    Puerto 5000

--- api/config/configuration.js
+++ api/config/configuration.js
@@ -1,6 +1,6 @@
 module.exports = {
     AUTHOR : 'Patrick Salguero',
-    PORT : 9000,
+    PORT : 5000,
     KEYCRYPT : 'cHNhbGd1ZXJv', //psalguero
     REGEX_PASSWORD : /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/ ,
     PATHUPLOAD : './api/uploads/'
